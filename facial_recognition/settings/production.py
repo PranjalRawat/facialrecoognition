8827e7efb5bb48b5b74f976dfa5b91e20d23d3bd
pranjalrawat <rawatpranjal111@gmail.com>
2020-10-16 12:55:00 +0530

    latest changes

--- facial_recognition/settings/production.py
+++ facial_recognition/settings/production.py
@@ -125,4 +125,4 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
